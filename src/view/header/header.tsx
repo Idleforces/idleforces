027fbd27a4f7920d636be371b96d8f1b8d30c38d
posij118 <schuttka@seznam.cz>
2022-12-12 01:37:12 +0800

    Add header

---
+++ src/view/header/header.tsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export const Header = (props: {leaveGame: () => void}) => {
+  const leaveGame = props.leaveGame;
+  return (
+    <header>
+      <a onClick={(_e) => leaveGame()} id="logo">
+        <img src="logo.svg" alt="Idleforces" />
+      </a>
+    </header>
+  );
+};
