bbf5644cdb936fd51c0bac425240b516af7fea79
posij118 <schuttka@seznam.cz>
2022-12-14 20:12:49 +0800

    Simplify problemSolveStatus type

--- src/app/problems/types.ts
+++ src/app/problems/types.ts
@@ -1,3 +1,5 @@
+import { generateProblem } from "./problem";
+
 export type ProblemPlacement = "A" | "B" | "C" | "D" | "E" | "F";
 export type ProblemDivision = 1 | 2 | 3 | 4;
 export enum ProblemTag {
@@ -9,103 +11,74 @@ export enum ProblemTag {
   trees = "trees",
 }
 
-export const problemBeforeReadingStatus = {
-  startedReading: false,
-  finishedReading: false,
-  ticksToFinishReading: null,
-};
+export type Problem = ReturnType<typeof generateProblem>;
 
 export type ProblemDuringReadingStatus = {
-  startedReading: true;
-  finishedReading: false;
   ticksToFinishReading: number;
 };
 
-export const problemAfterReadingStatus = {
-  startedReading: true,
-  finishedReading: true,
-  ticksToFinishReading: null,
-};
-
-export type ProblemReadingStatus =
-  | ProblemDuringReadingStatus
-  | typeof problemBeforeReadingStatus
-  | typeof problemAfterReadingStatus;
-
-export const problemBeforePenPaperSolvingStatus = {
-  startedPenPaperSolving: false,
-  finishedPenPaperSolving: false,
-  penPaperSolvingProgress: null,
-  penPaperSolvingIncrement: null,
-  ticksToPenPaperSolve: null,
-  isPenPaperCorrect: null,
-};
-
 export type ProblemDuringPenPaperSolvingStatus = {
-  startedPenPaperSolving: true;
-  finishedPenPaperSolving: false;
   penPaperSolvingProgress: number;
   penPaperSolvingIncrement: number;
   ticksToPenPaperSolve: number;
-  isPenPaperCorrect: null;
-};
-
-export type ProblemAfterPenPaperSolvingStatus = {
-  startedPenPaperSolving: true;
-  finishedPenPaperSolving: true;
-  penPaperSolvingProgress: null;
-  penPaperSolvingIncrement: null;
-  ticksToPenPaperSolve: null;
-  isPenPaperCorrect: boolean;
-};
-
-export type ProblemPenPaperStatus =
-  | ProblemDuringPenPaperSolvingStatus
-  | typeof problemBeforePenPaperSolvingStatus
-  | ProblemAfterPenPaperSolvingStatus;
-
-export const problemBeforeImplementingStatus = {
-  startedImplementing: false,
-  finishedImplementing: false,
-  implementingProgress: null,
-  implementingIncrement: null,
-  TicksToImplement: null,
-  isImplementationCorrect: null,
 };
 
 export type ProblemDuringImplementingStatus = {
-  startedImplementing: true;
-  finishedImplementing: false;
   implementingProgress: number;
   implementingIncrement: number;
-  TicksToImplement: number;
-  isImplementationCorrect: null;
+  ticksToImplement: number;
 };
 
-export type ProblemAfterImplementingStatus = {
-  startedImplementing: true;
-  finishedImplementing: true;
-  implementingProgress: null;
-  implementingIncrement: null;
-  TicksToImplement: null;
-  isImplementationCorrect: boolean;
+export type ProblemSolveStatusBeforeReading = {
+  phase: "before-reading";
 };
 
-export type ProblemImplementationStatus =
-  | ProblemDuringImplementingStatus
-  | typeof problemBeforeImplementingStatus
-  | ProblemAfterImplementingStatus;
-
-export type ProblemSolveStatus = (
-  | (typeof problemBeforeReadingStatus &
-      typeof problemBeforePenPaperSolvingStatus &
-      typeof problemBeforeImplementingStatus)
-  | (ProblemDuringReadingStatus &
-      typeof problemBeforePenPaperSolvingStatus &
-      typeof problemBeforeImplementingStatus)
-  | (typeof problemAfterReadingStatus &
-    ProblemPenPaperStatus &
-    ProblemImplementationStatus)
-) & {
-  wrongSubmissions: number;
+export type ProblemSolveStatusDuringReading = ProblemDuringReadingStatus & {
+  phase: "during-reading";
 };
+
+export type ProblemSolveStatusDuringPenPaperSolving =
+  ProblemDuringPenPaperSolvingStatus & {
+    phase: "during-pen-paper-solving";
+  };
+
+export type ProblemSolveStatusDuringImplementing =
+  ProblemDuringImplementingStatus & {
+    phase: "during-implementing";
+  };
+
+export type ProblemSolveStatusDuringResolving =
+  ProblemDuringPenPaperSolvingStatus &
+    ProblemDuringImplementingStatus & {
+      submissionTimestamps: Array<number>;
+      phase: "during-resolving";
+      lastSubmissionPenPaperCorrect: boolean;
+      lastSubmissionImplementationCorrect: boolean;
+    };
+
+export type ProblemSolveStatusAfterPassingPretests = {
+  submissionTimestamps: Array<number>;
+  phase: "after-passing-pretests";
+  lastSubmissionPenPaperCorrect: boolean;
+  lastSubmissionImplementationCorrect: boolean;
+};
+
+export type ProblemSolveStatusAfterPassingSystests = {
+  submissionTimestamps: Array<number>;
+  phase: "after-passing-systests";
+};
+
+export type ProblemSolveStatusAfterFailingSystests = {
+  submissionTimestamps: Array<number>;
+  phase: "after-failing-systests";
+};
+
+export type ProblemSolveStatus =
+  | ProblemSolveStatusBeforeReading
+  | ProblemSolveStatusDuringReading
+  | ProblemSolveStatusDuringPenPaperSolving
+  | ProblemSolveStatusDuringImplementing
+  | ProblemSolveStatusDuringResolving
+  | ProblemSolveStatusAfterPassingPretests
+  | ProblemSolveStatusAfterPassingSystests
+  | ProblemSolveStatusAfterFailingSystests;
