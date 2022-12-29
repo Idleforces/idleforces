328ee168d6871243001a559c686fcfdd17ad4648
posij118 <schuttka@seznam.cz>
2022-12-29 17:51:30 +0800

    Add game saving icon

--- src/view/game/navbar.tsx
+++ src/view/game/navbar.tsx
@@ -1,21 +1,31 @@
 import { NavLink, useLocation } from "react-router-dom";
 import "./navbar.css";
 
-export const NavBar = () => {
+export const NavBar = (props: { gameSaving: boolean }) => {
   const location = useLocation();
   const pages = ["dashboard"];
 
   return (
     <nav>
-      {pages.map((page) => (
-        <NavLink
-          to={`/game/${page}`}
-          className={location.pathname.includes(page) ? "current" : ""}
-          key={page}
-        >
-          {page}
-        </NavLink>
-      ))}
+      <div id="navlinks-container">
+        {pages.map((page) => (
+          <NavLink
+            to={`/game/${page}`}
+            className={location.pathname.includes(page) ? "current" : ""}
+            key={page}
+          >
+            {page}
+          </NavLink>
+        ))}
+      </div>
+      {props.gameSaving ? (
+        <>
+          <i className="fa-solid fa-spinner"></i>
+          <span>Loading...</span>
+        </>
+      ) : (
+        <></>
+      )}
     </nav>
   );
 };
