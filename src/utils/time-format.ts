5c333e31c821a6890cd7de91f782620fba703266
posij118 <schuttka@seznam.cz>
2022-12-29 20:28:16 +0800

    Fix convert to HH:MM function

--- src/utils/time-format.ts
+++ src/utils/time-format.ts
@@ -1,7 +1,3 @@
 export const convertSecondsToHHMM = (seconds: number): string => {
-  return new Date(seconds * 1000).toLocaleTimeString("en-US", {
-    hour: "numeric",
-    hour12: true,
-    minute: "numeric",
-  });
+  return new Date(seconds * 1000).toUTCString().slice(17, 22);
 };
