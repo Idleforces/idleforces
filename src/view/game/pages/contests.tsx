814959d5e7ae605f37b0c46a7f805de8bbc7e747
posij118 <schuttka@seznam.cz>
2022-12-31 14:52:42 +0800

    Add contests page

--- src/view/game/pages/contests.tsx
+++ src/view/game/pages/contests.tsx
@@ -4,6 +4,8 @@ import type { Dispatch, SetStateAction } from "react";
 import {
   CONTEST_LENGTH,
   DIVISION_MERGE_TICKS_COUNT,
+  MIN_USERS_SATISFYING_RATING_BOUND_TO_START_CONTEST,
+  SIMULATED_CONTESTS_MERGE_TICKS_COUNT,
 } from "../../../app/contest/constants";
 import { startContest } from "../../../app/contest/contest-slice";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
@@ -16,28 +18,33 @@ import type { ContestTypeRunning } from "../types";
 import { DataTable } from "../utils/datatable";
 import "./contests.css";
 import "../utils/datatable.css";
+import { setEventsToEmptyArray } from "../../../app/events/events-slice";
+import { filterUsersSatisfyingRatingBound } from "../../../app/contest/rating-bounds";
+import { Link } from "react-router-dom";
 
 const handleContestStart = (
   playerParticipating: boolean,
-  index: number,
+  rowIndex: number,
   dispatch: ThunkDispatch<RootState, void, AnyAction>,
   users: Array<User> | null,
   setContestTypeRunning: Dispatch<SetStateAction<ContestTypeRunning>>
 ) => {
   dispatch(
     startContest({
-      division: (4 - index) as ProblemDivision,
+      division: (4 - rowIndex) as ProblemDivision,
       playerParticipating,
-      numberOfMergedTicks:
-        DIVISION_MERGE_TICKS_COUNT[(4 - index) as ProblemDivision],
+      numberOfMergedTicks: playerParticipating
+        ? DIVISION_MERGE_TICKS_COUNT[(4 - rowIndex) as ProblemDivision]
+        : SIMULATED_CONTESTS_MERGE_TICKS_COUNT,
       users,
     })
   );
-
+  dispatch(setEventsToEmptyArray(null));
   setContestTypeRunning({
     playerParticipating,
-    numberOfMergedTicks:
-      DIVISION_MERGE_TICKS_COUNT[(4 - index) as ProblemDivision],
+    numberOfMergedTicks: playerParticipating
+      ? DIVISION_MERGE_TICKS_COUNT[(4 - rowIndex) as ProblemDivision]
+      : SIMULATED_CONTESTS_MERGE_TICKS_COUNT,
   });
 };
 
@@ -48,62 +55,64 @@ export const Contests = (props: {
   const contestTypeRunning = props.contestTypeRunning;
   const setContestTypeRunning = props.setContestTypeRunning;
   const dispatch = useAppDispatch();
-  const users = useAppSelector(selectUsers);
+  const users = useAppSelector(selectUsers) ?? [];
+  const usersSatisfyingRatingBoundsCounts = (
+    [4, 3, 2, 1] as Array<ProblemDivision>
+  ).map((division) => filterUsersSatisfyingRatingBound(users, division).length);
 
   if (contestTypeRunning)
     return (
       <div id="contests-page" style={{ fontSize: "1.5rem" }}>
         <div>A contest is running.</div>
-        <a href="/game/contest/dashboard">Return to contest page.</a>
+        <Link to="/game/contest/problems">Return to contest page.</Link>
       </div>
     );
+
+  const computeStartContestFieldContent = (
+    rowIndex: number,
+    playerParticipating: boolean
+  ): JSX.Element => {
+    return usersSatisfyingRatingBoundsCounts[rowIndex] >=
+      MIN_USERS_SATISFYING_RATING_BOUND_TO_START_CONTEST ? (
+      <a
+        className="dark-red-link"
+        onClick={() => {
+          handleContestStart(
+            playerParticipating,
+            rowIndex,
+            dispatch,
+            users,
+            setContestTypeRunning
+          );
+        }}
+      >
+        {playerParticipating ? "Register" : "Simulate"}
+      </a>
+    ) : (
+      <span style={{ color: "#222" }}>Not enough users can participate.</span>
+    );
+  };
 
   const dataTableContents: Array<Array<JSX.Element>> = [
     [<>Name</>, <>Cooldown</>, <>Length</>, <>Start</>, <>Simulate</>],
   ].concat(
     Array(4)
       .fill(0)
-      .map((_, index) => [
-        <>Idleforces Round (Div. {4 - index})</>,
+      .map((_, rowIndex) => [
+        <>Idleforces Round (Div. {4 - rowIndex})</>,
         <></>,
         <>
           {convertSecondsToHHMM(
             CONTEST_LENGTH /
-              DIVISION_MERGE_TICKS_COUNT[(4 - index) as ProblemDivision]
+              DIVISION_MERGE_TICKS_COUNT[(4 - rowIndex) as ProblemDivision]
           )}
         </>,
-        <a
-        className="dark-red-link"
-          onClick={() => {
-            handleContestStart(
-              true,
-              index,
-              dispatch,
-              users,
-              setContestTypeRunning
-            );
-          }}
-        >
-          Enter
-        </a>,
-        <a
-          className="dark-red-link"
-          onClick={() => {
-            handleContestStart(
-              false,
-              index,
-              dispatch,
-              users,
-              setContestTypeRunning
-            );
-          }}
-        >
-          Simulate
-        </a>,
+        computeStartContestFieldContent(rowIndex, true),
+        computeStartContestFieldContent(rowIndex, false),
       ])
-  );
+    );
 
-  const extraClassNames = Array(5)
+  const classNames = Array(5)
     .fill(0)
     .map((_, index) =>
       index === 0
@@ -115,7 +124,7 @@ export const Contests = (props: {
     <div id="contests-page">
       <DataTable
         contents={dataTableContents}
-        extraClassNames={extraClassNames}
+        classNames={classNames}
         containerBorderRadiusPx={6}
         topText={"Current or upcoming contests"}
       />
