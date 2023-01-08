44b19e5ebde93af5ca32014e844f198b6276ce82
posij118 <schuttka@seznam.cz>
2023-01-08 14:25:08 +0800

    Fix browser making repeated requests for flags

--- src/view/game/utils/flag.tsx
+++ src/view/game/utils/flag.tsx
@@ -1,11 +1,42 @@
 import { findFlagUrlByCountryName } from "country-flags-svg";
 
+const countryNamesWhitelist: Array<string> = [];
+const countryNamesBlacklist: Array<string> = [];
+
 export const Flag = (props: { countryName: string | null }) => {
   const countryName = props.countryName;
 
-  if (countryName === null) return <></>;
-  const flagURL = findFlagUrlByCountryName(countryName);
-  if (!flagURL) return <></>;
+  if (countryName === null || countryNamesBlacklist.includes(countryName))
+    return <></>;
 
-  return <img src={flagURL} alt={countryName} height="16px" width="auto" style={{paddingRight: "5px"}}/>;
+  const flagURL = findFlagUrlByCountryName(countryName);
+  if (flagURL === null || flagURL === "") {
+    if (!countryNamesBlacklist.includes(countryName))
+      countryNamesBlacklist.push(countryName);
+    return <></>;
+  }
+
+  if (
+    !countryNamesBlacklist.includes(countryName) &&
+    !countryNamesWhitelist.includes(countryName)
+  ) {
+    fetch(flagURL)
+      .then((response) => {
+        if (response.status === 200) countryNamesWhitelist.push(countryName);
+        else countryNamesBlacklist.push(countryName);
+      })
+      .catch((err: unknown) => {
+        console.warn(err);
+      });
+  }
+
+  return (
+    <img
+      src={flagURL}
+      alt={countryName}
+      height="16px"
+      width="auto"
+      style={{ paddingRight: "5px" }}
+    />
+  );
 };
