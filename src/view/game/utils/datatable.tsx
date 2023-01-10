00d5c1bfb4da0b6779beb3c96eb5ba67515c717f
posij118 <schuttka@seznam.cz>
2023-01-10 15:29:18 +0800

    Add more flexibility to datatable

--- src/view/game/utils/datatable.tsx
+++ src/view/game/utils/datatable.tsx
@@ -2,16 +2,21 @@ export const DataTable = (props: {
   topText?: string;
   containerBorderRadiusPx?: number;
   contents: Array<Array<JSX.Element>>;
-  classNames: Array<Array<string>>;
+  classNames?: Array<Array<string>>;
+  tableId?: string
+  containerId?: string
 }) => {
   const contents = props.contents;
   const classNames = props.classNames;
   const topText = props.topText;
   const containerBorderRadiusPx = props.containerBorderRadiusPx ?? 0;
+  const tableId = props.tableId;
+  const containerId = props.containerId;
 
   return (
     <div
       className="datatable"
+      id={containerId}
       style={{
         paddingRight: (3 / 2) * containerBorderRadiusPx,
         paddingLeft: containerBorderRadiusPx / 2,
@@ -22,6 +27,7 @@ export const DataTable = (props: {
     >
       {topText !== undefined ? <div className="header">{topText}</div> : <></>}
       <table
+        id={tableId}
         style={{
           margin: containerBorderRadiusPx / 2,
         }}
@@ -31,7 +37,7 @@ export const DataTable = (props: {
             {contents[0].map((cellContent, columnIndex) => (
               <th
                 key={columnIndex}
-                className={`top ${classNames[0][columnIndex]} ${
+                className={`top ${classNames ? classNames[0][columnIndex] : ""} ${
                   columnIndex === 0 ? "left" : ""
                 } ${columnIndex === contents[0].length - 1 ? "right" : ""}`}
               >
@@ -46,7 +52,7 @@ export const DataTable = (props: {
               {rowContents.map((cellContent, columnIndex) => (
                 <td
                   key={columnIndex}
-                  className={`${classNames[rowIndex + 1][columnIndex]} ${
+                  className={`${classNames ? classNames[rowIndex + 1][columnIndex] : ""} ${
                     columnIndex === 0 ? "left" : ""
                   } ${columnIndex === rowContents.length - 1 ? "right" : ""}
                   ${rowIndex === contents.length - 1 ? "down" : ""}
