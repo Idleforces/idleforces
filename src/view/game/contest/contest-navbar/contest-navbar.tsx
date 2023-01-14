383f109336c3fbf44eab8d66f4cffe677e4288b7
posij118 <schuttka@seznam.cz>
2023-01-14 21:49:29 +0800

    Add contest sub-navbar logic

--- src/view/game/contest/contest-navbar/contest-navbar.tsx
+++ src/view/game/contest/contest-navbar/contest-navbar.tsx
@@ -1,20 +1,55 @@
+import { useState } from "react";
+import type { Dispatch, SetStateAction} from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "../../navbar.css";
+import type { RatingRecomputeData } from "../standings/standings";
 
-export const ContestNavBar = () => {
+const computePageUrl = (page: string) => {
+  return page === "friends standings"
+    ? "/game/contest/standings/friends"
+    : `/game/contest/${page}`;
+};
+
+const computeIfInitialSelected = (
+  locationPathname: string,
+  pageName: string
+) => {
+  const pageNameRegex = new RegExp(
+    pageName === "friends standings" ? "friends" : `${pageName}$`
+  );
+  return locationPathname.match(pageNameRegex) !== null;
+};
+
+export const ContestNavBar = (props: {
+  setRatingRecomputeData: Dispatch<SetStateAction<RatingRecomputeData>>
+}) => {
+  const {setRatingRecomputeData} = props;
   const location = useLocation();
-  const pages = ["problems", "standings"];
+  const pages = ["problems", "standings", "friends standings"];
+
+  const [selected, setSelected] = useState(
+    pages.map((page) => computeIfInitialSelected(location.pathname, page))
+  );
 
   return (
-    <nav>
-      <div id="navlinks-container" style={{
-
-      }}>
-        {pages.map((page) => (
+    <nav className="sub-navbar">
+      <div id="navlinks-container">
+        {pages.map((page, index) => (
           <NavLink
-            to={`/game/contest/${page}`}
-            className={location.pathname.includes(page) ? "current" : ""}
+            to={`${computePageUrl(page)}`}
+            className={selected[index] ? "current" : ""}
             key={page}
+            onMouseOver={(_e) => {
+              setSelected(pages.map((menuPage) => menuPage === page));
+            }}
+            onMouseOut={(_e) => {
+              setSelected(
+                pages.map((page) =>
+                  computeIfInitialSelected(location.pathname, page)
+                )
+              );
+            }}
+            onClick={(_e) => { setRatingRecomputeData({placeholder: true}); }}
           >
             {page}
           </NavLink>
