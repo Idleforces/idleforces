e856bb123ced8f633c1cb8c9b96b97570f9b4cfe
posij118 <schuttka@seznam.cz>
2023-01-16 20:14:18 +0800

    Add fine-grained datatable controls

--- src/view/game/utils/datatable.tsx
+++ src/view/game/utils/datatable.tsx
@@ -1,10 +1,16 @@
+export type AllowableTableClassNames = "datatable-no-outer-border"
+export type AllowableTableIds = never;
+export type AllowableContainerIds = never;
+
 export const DataTable = (props: {
   topText?: string;
   containerBorderRadiusPx?: number;
   contents: Array<Array<JSX.Element>>;
   classNames?: Array<Array<string>>;
-  tableId?: string
-  containerId?: string
+  tableClassName?: AllowableTableClassNames;
+  tableId?: AllowableTableIds;
+  containerId?: AllowableContainerIds;
+  colspanValues?: Array<Array<number>>
 }) => {
   const contents = props.contents;
   const classNames = props.classNames;
@@ -12,6 +18,8 @@ export const DataTable = (props: {
   const containerBorderRadiusPx = props.containerBorderRadiusPx ?? 0;
   const tableId = props.tableId;
   const containerId = props.containerId;
+  const tableClassName = props.tableClassName ?? "";
+  const colspanValues = props.colspanValues;
 
   return (
     <div
@@ -28,6 +36,7 @@ export const DataTable = (props: {
       {topText !== undefined ? <div className="header">{topText}</div> : <></>}
       <table
         id={tableId}
+        className={tableClassName.trim()}
         style={{
           margin: containerBorderRadiusPx / 2,
         }}
@@ -37,9 +46,12 @@ export const DataTable = (props: {
             {contents[0].map((cellContent, columnIndex) => (
               <th
                 key={columnIndex}
-                className={`top ${classNames ? classNames[0][columnIndex] : ""} ${
-                  columnIndex === 0 ? "left" : ""
-                } ${columnIndex === contents[0].length - 1 ? "right" : ""}`}
+                colSpan={colspanValues ? colspanValues[0][columnIndex] : undefined}
+                className={`top ${
+                  classNames ? classNames[0][columnIndex] : ""
+                } ${columnIndex === 0 ? "left" : ""} ${
+                  columnIndex === contents[0].length - 1 ? "right" : ""
+                }`.trim()}
               >
                 {cellContent}
               </th>
@@ -52,11 +64,14 @@ export const DataTable = (props: {
               {rowContents.map((cellContent, columnIndex) => (
                 <td
                   key={columnIndex}
-                  className={`${classNames ? classNames[rowIndex + 1][columnIndex] : ""} ${
-                    columnIndex === 0 ? "left" : ""
-                  } ${columnIndex === rowContents.length - 1 ? "right" : ""}
+                  colSpan={colspanValues ? colspanValues[rowIndex + 1][columnIndex] : undefined}
+                  className={`${
+                    classNames ? classNames[rowIndex + 1][columnIndex] : ""
+                  } ${columnIndex === 0 ? "left" : ""} ${
+                    columnIndex === rowContents.length - 1 ? "right" : ""
+                  }
                   ${rowIndex === contents.length - 1 ? "down" : ""}
-                  ${rowIndex % 2 === 0 ? "dark" : ""}`}
+                  ${rowIndex % 2 === 0 ? "dark" : ""}`.trim()}
                 >
                   {cellContent}
                 </td>
