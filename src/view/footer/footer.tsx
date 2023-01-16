2dea32d42d71d12d7e42d994f4b57f0c7e28beea
posij118 <schuttka@seznam.cz>
2023-01-16 20:12:24 +0800

    Refactor styling and margins

--- src/view/footer/footer.tsx
+++ src/view/footer/footer.tsx
@@ -4,13 +4,21 @@ import { selectTimeOfSnapshot } from "../../app/users/users-slice";
 export const Footer = () => {
   const timeOfSnapshot = useAppSelector(selectTimeOfSnapshot);
   return (
-    <>
-      <p style={{fontSize: "0.75rem", margin: "2rem 10% 1rem"}}>
-      If you have been inspired by this silly game and want to grind IRL, then Codeforces is the platform for you.
+    <footer>
+      <p
+        style={{ fontSize: "0.75rem", marginTop: "2rem", marginBottom: "1rem" }}
+      >
+        If you have been inspired by this silly game and want to grind IRL, then
+        Codeforces is the platform for you.
       </p>
-      <p style={{fontSize: "0.75rem", margin: "0 10%"}}>
-      *This game uses real Codeforces handles provided by the Codeforces API. Their performance in this game depends only on their real rating{timeOfSnapshot !== null ? ` snapshotted at ${new Date(timeOfSnapshot).toString()}` : "" }.
+      <p style={{ fontSize: "0.75rem" }}>
+        *This game uses real Codeforces handles provided by the Codeforces API.
+        Their performance in this game depends only on their real rating
+        {timeOfSnapshot !== null
+          ? ` snapshotted at ${new Date(timeOfSnapshot).toString()}`
+          : ""}
+        .
       </p>
-    </>
+    </footer>
   );
 };
