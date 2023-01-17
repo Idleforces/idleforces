65ab61c9e01509be1008a2574ec53c3436f323c0
posij118 <schuttka@seznam.cz>
2023-01-17 17:47:06 +0800

    Make sub-navbar clicks generally restore defaults

--- src/view/game/sub-navbar.tsx
+++ src/view/game/sub-navbar.tsx
@@ -3,36 +3,100 @@ import type { Dispatch, SetStateAction } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./navbar.css";
 import type { RatingRecomputeData } from "./contest/standings/standings";
+import type { ContestSubmissionsFilterData } from "./contest/status/status";
+import { useAppSelector } from "../../app/hooks";
+import { selectHandle } from "../../app/save/save-slice";
 
-const computePageUrl = (baseURL: string, page: string, handle?: string) => {
+function computePageUrl<T extends string>(
+  baseURL: string,
+  page: T,
+  handle?: string
+) {
   const pageURL =
     page === "friends standings"
       ? `${baseURL}standings/friends`
       : page === "contests" && handle !== undefined
       ? `${baseURL}${handle}/${page}`
+      : page === "my submissions"
+      ? `${baseURL}my`
       : `${baseURL}${page}`;
 
   return pageURL;
-};
+}
 
-const computeIfInitialSelected = (
+function computeIfInitialSelected<T extends string>(
   locationPathname: string,
-  pageName: string
-) => {
+  pageName: T
+) {
   const pageNameRegex = new RegExp(
-    pageName === "friends standings" ? "friends" : `${pageName}$`
+    pageName === "friends standings"
+      ? "friends"
+      : pageName === "my submissions"
+      ? "my$"
+      : `${pageName}$`
   );
   return locationPathname.match(pageNameRegex) !== null;
-};
+}
 
-export const SubNavBar = (props: {
+function handleNavBarClickSideEffects<T extends string>(data: {
+  page: T;
+  setRatingRecomputeData?: Dispatch<SetStateAction<RatingRecomputeData>>;
+  setContestSubmissionsFilterData?: Dispatch<
+    SetStateAction<ContestSubmissionsFilterData>
+  >;
+  playerHandle?: string;
+}) {
+  const {
+    page,
+    setRatingRecomputeData,
+    setContestSubmissionsFilterData,
+    playerHandle,
+  } = data;
+
+  if (setRatingRecomputeData) {
+    setRatingRecomputeData({ placeholder: true });
+  }
+
+  if (
+    page === "my submissions" &&
+    setContestSubmissionsFilterData &&
+    playerHandle !== undefined
+  ) {
+    setContestSubmissionsFilterData({
+      handle: playerHandle,
+      problemPlacement: null,
+      verdict: null,
+    });
+  }
+
+  if (page === "status" && setContestSubmissionsFilterData) {
+    setContestSubmissionsFilterData({
+      handle: "",
+      problemPlacement: null,
+      verdict: null,
+    });
+  }
+}
+
+export function SubNavBar<T extends string>(props: {
   baseURL: string;
-  pages: Array<string>;
+  pages: Array<T>;
   handle?: string;
   setRatingRecomputeData?: Dispatch<SetStateAction<RatingRecomputeData>>;
-}) => {
-  const { setRatingRecomputeData, pages, baseURL, handle } = props;
+  setContestSubmissionsFilterData?: Dispatch<
+    SetStateAction<ContestSubmissionsFilterData>
+  >;
+}) {
+  const {
+    setRatingRecomputeData,
+    pages,
+    baseURL,
+    handle,
+    setContestSubmissionsFilterData,
+  } = props;
+
   const location = useLocation();
+  const playerHandle = useAppSelector(selectHandle);
 
   const [selected, setSelected] = useState(
     pages.map((page) => computeIfInitialSelected(location.pathname, page))
@@ -56,13 +120,24 @@ export const SubNavBar = (props: {
                 )
               );
             }}
-            onClick={
-              setRatingRecomputeData
-                ? (_e) => {
-                    setRatingRecomputeData({ placeholder: true });
-                  }
-                : undefined
-            }
+            onClick={(_e) => {
+              handleNavBarClickSideEffects({
+                page,
+                setRatingRecomputeData,
+                setContestSubmissionsFilterData,
+                playerHandle,
+              });
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleNavBarClickSideEffects({
+                  page,
+                  setRatingRecomputeData,
+                  setContestSubmissionsFilterData,
+                  playerHandle,
+                });
+              }
+            }}
           >
             {page}
           </NavLink>
@@ -70,4 +145,4 @@ export const SubNavBar = (props: {
       </div>
     </nav>
   );
-};
+}
