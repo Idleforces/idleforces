d95f7c19446eaa7b06f546103fd7faa52f30547f
posij118 <schuttka@seznam.cz>
2023-01-29 16:59:50 +0800

    Round diff to two decimal places

--- src/view/game/utils/formatted-diff.tsx
+++ src/view/game/utils/formatted-diff.tsx
@@ -1,8 +1,10 @@
 export const FormattedDiff = (props: { diff: number }) => {
-  const { diff } = props;
+  const diff = Math.round(100 * props.diff) / 100;
 
   return (
-    <div className={diff > 0 ? "green bold" : diff < 0 ? "red bold" : ""}>
+    <div
+      className={diff > 0 ? "dark-green bold" : diff < 0 ? "dark-red bold" : ""}
+    >
       {diff > 0 ? `+${diff}` : diff}
     </div>
   );
